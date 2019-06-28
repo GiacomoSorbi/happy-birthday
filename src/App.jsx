7196bb925b1f8effa08465a910a7ca5c582a664f
Giacomo Sorbi <giacomo.sorbi@bjss.com>
2019-06-28 11:48:24 +0100

    added emojis for all the missing options

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span role="img" label="Birthday Cake">
+            🎂
+          </span>
+          Welcome... What is your name?
+          <span role="img" label="Birthday Cake">
+            🎂
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>📅</p>
       </header>
     </div>
   );
