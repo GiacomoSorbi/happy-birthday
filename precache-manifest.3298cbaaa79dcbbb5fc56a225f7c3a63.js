2e4d0a233bce56357a29065c5e2b166358f09559
Giacomo Sorbi <giacomo.sorbi@bjss.com>
2019-06-30 22:48:35 +0100

    Updates

---
+++ precache-manifest.3298cbaaa79dcbbb5fc56a225f7c3a63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d36e004eae076a651fbfd35176a790d",
+    "url": "/happy-birthday/index.html"
+  },
+  {
+    "revision": "eb1b59673878a88d099a",
+    "url": "/happy-birthday/static/css/main.53ae592e.chunk.css"
+  },
+  {
+    "revision": "6f9cf9382391778539f5",
+    "url": "/happy-birthday/static/js/2.61716f1a.chunk.js"
+  },
+  {
+    "revision": "eb1b59673878a88d099a",
+    "url": "/happy-birthday/static/js/main.b74ab38e.chunk.js"
+  },
+  {
+    "revision": "f984a2eaddb9e8d5ee2c",
+    "url": "/happy-birthday/static/js/runtime~main.b1b52b95.js"
+  },
+  {
+    "revision": "320014c1ea483f54625485973c1f7cc2",
+    "url": "/happy-birthday/static/media/firework0.320014c1.mp3"
+  },
+  {
+    "revision": "8c9363400b57f3c8b26622970ab79a37",
+    "url": "/happy-birthday/static/media/firework1.8c936340.mp3"
+  },
+  {
+    "revision": "56e0141fd0bb82febcbe06fb5ba75a56",
+    "url": "/happy-birthday/static/media/firework2.56e0141f.mp3"
+  },
+  {
+    "revision": "1f8c7856dd0b9f95fc09a4dae1366b17",
+    "url": "/happy-birthday/static/media/firework3.1f8c7856.mp3"
+  }
+]);
